09b87eb4018eaf4b974dc5fdafe33c1a17eeb206
Lipika <lipikapande@gmail.com>
2025-03-29 13:42:43 +0530

    prac

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ console.log(ShoppingCart.totalPrice);
 
 import add from './shoppingCart.js';
 add('pizza', 2);
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await res.json();
+  // console.log(data);
+  return { title: data.id, text: data.completed };
+};
+
+//not clean
+// const lastPost = getLastPost(); //will return promise
+// console.log(lastPost);
+
+// lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
